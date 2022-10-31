69677446b914ad88ff674d1fcf629f27ad84d417
shastasubramanian <shasta.subramanian@gmail.com>
2022-10-30 21:53:47 -0700

    finalized explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,27 +1,58 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
-
 function init() {
   // TODO
   console.log("Starting");
   const synth = window.speechSynthesis;
   const msg = new SpeechSynthesisUtterance();
-  var voices = synth.getVoices();
-  console.log(voices.length);
+  const voiceSelect = document.querySelector('select');
+  let voices = [];
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  var voice_select = document.getElementById("voice-select");
+  voiceSelect.addEventListener("change",(event)=>{
+    msg.voiceURI = voiceSelect.options[voiceSelect.selectedIndex].getAttribute("data-name");
+    msg.lang = voiceSelect.options[voiceSelect.selectedIndex].getAttribute("data-lang");
+  }); 
   msg.volume = 1;
   msg.rate = 1;
   msg.pitch = 1;
-  msg.text = "Hello, my name is Shasta";
-  msg.voiceURI = "Alex";
-  msg.lang = "en-US";
-  
+  let text = document.querySelector("#text-to-speak");
   let button = document.querySelector("button");
+  text.addEventListener('input', updateValue);
   const input = document.querySelector('input');
   button.addEventListener('click',(event) => {
-    msg.text = "text-to-speak";
+    msg.text = text.value;
+    openFace();
     speechSynthesis.speak(msg);
+    msg.addEventListener('end', (event)=>{
+      closeFace();
+    });
   });
 };
-
-
+function updateValue(e) {
+  console.log(e.target.value);
+}
+function openFace(){
+  let img_select = document.querySelector("#explore > img");
+  img_select.src = "assets/images/smiling-open.png";
+}
+function closeFace(){
+  let img_select = document.querySelector("#explore > img");
+  img_select.src = "assets/images/smiling.png";
+}
